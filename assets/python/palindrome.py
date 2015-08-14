e52e7ce864df22acb7895bd859260f923aaf2107
Leo Mehr <ljm262@cornell.edu>
2015-08-13 22:15:51 -0700

    https support

--- assets/python/palindrome.py
+++ assets/python/palindrome.py
@@ -1,7 +1,7 @@
 def is_pal(word):
     return word == word[::-1]
 
-words = 'Hello! I am a robot racecar.'.split()
+words = 'Hello! I am a robot racecar'.split()
 
 for (w,p) in zip(words, map(is_pal, words)):
     print repr(w) + ':', p
